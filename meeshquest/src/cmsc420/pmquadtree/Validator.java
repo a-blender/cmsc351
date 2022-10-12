3084d90b9408d3c52b6905afbc88618a53d0ad88
Anna Blendermann <anna.blendermann@suse.com>
2022-10-12 18:37:41 -0400

    Removed simple projs

--- meeshquest/src/cmsc420/pmquadtree/Validator.java
+++
@@ -1,8 +0,0 @@
-package cmsc420.pmquadtree;
-
-import cmsc420.pmquadtree.PMQuadtree.Black;
-
-public interface Validator {
-
-	public boolean valid(Black node);
-}
--- meeshquest/src/cmsc420/sortedmap/StringComparator.java
+++
@@ -1,9 +0,0 @@
-package cmsc420.sortedmap;
-
-import java.util.Comparator;
-
-public class StringComparator implements Comparator<String> {
-    public int compare(final String s1, final String s2) {
-        return s1.compareTo(s2);
-    }
-}
